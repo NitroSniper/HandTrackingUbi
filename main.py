107b473033b8bf1e4d90d0c2e69067a546e67af5
NitroSniper <mahiemiah35@gmail.com>
2025-02-11 03:10:47 +0000

    Video tracking

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+
 import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -5,29 +7,36 @@ from mediapipe.tasks.python import vision
 from utilities import draw_landmarks_on_image
 
 
+annotated_image = None
+
+def print_result(result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    global annotated_image
+    # print('hand landmarker result: {}'.format(result))
+    annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
+
 def main():
     capture = cv2.VideoCapture(0)
 
     base_options = mp.tasks.BaseOptions(model_asset_path='hand_landmarker.task')
-    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-    detector = vision.HandLandmarker.create_from_options(options)
+    options = vision.HandLandmarkerOptions(
+        base_options=base_options, num_hands=2, running_mode=vision.RunningMode.LIVE_STREAM,
+        result_callback=print_result
+    )
 
+    with vision.HandLandmarker.create_from_options(options) as detector:
+        while cv2.waitKey(1) != ord('q'):
+            _, frame = capture.read()
 
-    while cv2.waitKey(1) != ord('q'):
-        _, frame = capture.read()
+            x, y, z = frame.shape
 
-        x, y, z = frame.shape
+            frame = cv2.flip(frame, 1)
+            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+            detector.detect_async(image, int(perf_counter()*1000))
 
-        frame = cv2.flip(frame, 1)
-        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        detection_result = detector.detect(image)
-
-        # STEP 5: Process the classification result. In this case, visualize it.
-        annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-        cv2.imshow("Picture", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-
-
-        cv2.imshow("Output", frame)
+            if annotated_image is not None:
+                cv2.imshow("Picture", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+            # STEP 5: Process the classification result. In this case, visualize it.
+            # cv2.imshow("Output", frame)
 
 
 
