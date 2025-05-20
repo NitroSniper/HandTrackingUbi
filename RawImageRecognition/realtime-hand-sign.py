95d0107dd5a33ab72d3ccde45c0bb025df69af79
NitroSniper <mahiemiah35@gmail.com>
2025-05-20 02:40:07 +0100

    KNN Hand Sign Model clean code

--- RawImageRecognition/realtime-hand-sign.py
+++ RawImageRecognition/realtime-hand-sign.py
@@ -4,17 +4,17 @@ import mediapipe as mp
 import pickle
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
-#%%
+
 # configuration
 model_path = "neigh-model.pkl"  # path to the saved model
 data_direct   = "images"
 image_size   = (244, 244) # resize image
 # dominant_hand = "Right" # can be changed (but need to train the new model)
-#%%
+
 # Load knn model and labelEncoder
 with open(model_path, 'rb') as f:
     model, label = pickle.load(f)
-#%%
+
 def process_raw_features(raw_features: mp.tasks.vision.HandLandmarkerResult):
     landmarks = []
     for hand_landmarks in results.hand_landmarks:
@@ -27,34 +27,33 @@ def process_raw_features(raw_features: mp.tasks.vision.HandLandmarkerResult):
         z_coordinates = np.array([landmark.z for landmark in hand_landmarks])
 
         landmarks.append(np.array([
-            [x, y, z] for x, y, z in zip(norm_x_coordinates, norm_y_coordinates,     z_coordinates)
+            [x, y, z] for x, y, z in zip(norm_x_coordinates, norm_y_coordinates, z_coordinates)
         ]).flatten())
     return landmarks
-#%%
-
 
 MARGIN = 10  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
-def draw_landmarks_on_image(rgb_image, detection_result, label):
+def draw_landmarks_on_image(rgb_image, detection_result, label, debug):
     annotated_image = np.copy(rgb_image)
 
     # Loop through the detected hands to visualize.
     for hand_landmarks, handedness in zip(detection_result.hand_landmarks, detection_result.handedness):
 
         # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        # solutions.drawing_utils.draw_landmarks(
-        #     annotated_image,
-        #     hand_landmarks_proto,
-        #     solutions.hands.HAND_CONNECTIONS,
-        #     solutions.drawing_styles.get_default_hand_landmarks_style(),
-        #     solutions.drawing_styles.get_default_hand_connections_style())
+        if debug:
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+                annotated_image,
+                hand_landmarks_proto,
+                solutions.hands.HAND_CONNECTIONS,
+                solutions.drawing_styles.get_default_hand_landmarks_style(),
+                solutions.drawing_styles.get_default_hand_connections_style())
 
         # Get the top left corner of the detected hand's bounding box.
         height, width, _ = annotated_image.shape
@@ -70,15 +69,14 @@ def draw_landmarks_on_image(rgb_image, detection_result, label):
 
     return annotated_image
 
-#%%
-def process_and_evaluate(raw_features: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image):
+def process_and_evaluate(raw_features: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, debug: bool):
     X = process_raw_features(raw_features)
     if len(X) == 0:
         return output_image.numpy_view()
     result = label.classes_[model.predict(X)]
-    annotated_image = draw_landmarks_on_image(output_image.numpy_view(), raw_features, result)
+    annotated_image = draw_landmarks_on_image(output_image.numpy_view(), raw_features, result, debug)
     return annotated_image
-#%%
+
 # load the trained model
 base_options = mp.tasks.BaseOptions(model_asset_path='hand_landmarker.task')
 options = mp.tasks.vision.HandLandmarkerOptions(
@@ -86,15 +84,14 @@ options = mp.tasks.vision.HandLandmarkerOptions(
     # result_callback=process_and_evaluate
 )
 
-#%%
 with mp.tasks.vision.HandLandmarker.create_from_options(options) as detector:
     capture = cv2.VideoCapture(0)
-    while cv2.waitKey(100) != ord('q'):
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, image_size[0])
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size[1])
+    while cv2.waitKey(10) != ord('q'):
         _, frame = capture.read()
-
         frame = cv2.flip(frame, 1)
         image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
         results = detector.detect(image)
-        output_image = process_and_evaluate(results, image)
-
+        output_image = process_and_evaluate(results, image, debug=False)
         cv2.imshow("Picture", output_image)
